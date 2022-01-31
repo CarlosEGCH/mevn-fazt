a3964698a02d272adf49b53206ca0650b89e8435
CarlosEGCH <carlitosgch4@gmail.com>
2022-01-31 18:05:18 +0000

    Created more tasks

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,24 @@
 import { Router } from "express";
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello World");
+router.get("/tasks", (req, res) => {
+    res.send("Getting tasks!");
+})
+
+router.post("/tasks", (req, res) => {
+    res.send("Creating tasks");
+})
+
+router.get("/tasks/:id", (req, res) => {
+    res.send("Getting ONE task!");
+})
+
+router.delete("/tasks/:id", (req, res) => {
+    res.send("Deleting ONE task");
+})
+
+router.put("/tasks/:id", (req, res) => {
+    res.send("Updating ONE task");
 })
 
 export default router;
